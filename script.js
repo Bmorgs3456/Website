f411ebf24fd3e720f946ea13b1b2a5f3ec5526de
Brandon Morgan <brandon.l.morgan@outlook.com>
2021-12-21 23:40:19 +0000

    Version 1.02

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-var today = new Date();
-
-var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-
-document.write(date);
-
-
-function(num1, num2) {
-	var x = num1;
-	var y = num2
-	var z = num1 + num2;
-	document.write(z);
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
 }
-document.write(3,5)
-
-function() {
-  var word = "scrabble";
-  word.length();
-  word.substr(5);
-  word.toUpperCase();
-  document.write(word);
-}
+setInterval(refreshTime, 1000);
